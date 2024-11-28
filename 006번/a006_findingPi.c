1eaa788197e4f831619e6049e4b2cfb45bd75501
nemo <nemo724@naver.com>
2024-11-28 23:52:37 +0900

    구한 파이값 중 앞의 10개, 끝의 5개의 결과를 기록하는 테이블 배열 report_pi 추가

--- 006번/a006_findingPi.c
+++ 006번/a006_findingPi.c
@@ -3,9 +3,12 @@
 
 
 
+
+
 int main(void)
-{bool minus = false;
-double pi = 0;
+{bool minus = false;//현재 더할 항의 부호가 -인가를 판단하는 변수
+double pi = 0;//라이프니츠 원주율 공식에서 n항까지의 누적 결과를 담는 변수
+double report_pi[15] = { 0 };//선두의 10개,끝의 5개의 파이 값을 기록하는 테이블
 
 
 
